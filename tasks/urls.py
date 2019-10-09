16008f6fea9c5314504ec5883f7503cfcb29e35d
Oliver Boamah <oliverboamah@yahoo.com>
2019-10-09 16:05:37 +0000

    Add update task feature

--- tasks/urls.py
+++ tasks/urls.py
@@ -8,5 +8,10 @@ app_name = 'tasks'
 
 urlpatterns = [
     path(route='', view=task_views.get_all_tasks, name='index'),
-    path(route='tasks/create', view=task_views.create_task, name='create')
+    path(route='tasks/create', view=task_views.create_task, name='create'),
+    path(route='tasks/task-detail/<int:id>', view=task_views.get_task, name='detail'),
+    path(route='tasks/update/<int:id>', view=task_views.update_task, name='update'),
+
+    # json get request
+    path(route='tasks/get-task-json/<int:id>', view=task_views.get_task_json)
 ]
--- tasks/views/task_views.py
+++ tasks/views/task_views.py
@@ -1,26 +1,21 @@
+# Core Python imports
+import json
+
 # Django imports
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.http import JsonResponse
 
 # My App imports
-from tasks.models.task_model import Task
 from tasks.forms.create_task_form import CreateTaskForm
+from tasks.forms.update_task_form import UpdateTaskForm
+from tasks.models.task_model import Task
 
 
-# retrieve all tasks
-def get_all_tasks(request):
-    data = {
-        'tasks': Task.objects.all(),
-        'store_task_form': CreateTaskForm()
-    }
-    return render(request, 'tasks/index.html', data)
-
-
-# store task
+# create task
 def create_task(request):
-    form = CreateTaskForm(request.POST)
-
     try:
+        form = CreateTaskForm(data=request.POST)
         form.is_valid()
         new_task = form.save()
         messages.success(request, f"Task '{new_task.title}' created successfully!")
@@ -28,3 +23,50 @@ def create_task(request):
         messages.error(request, e)
 
     return redirect('tasks:index')
+
+
+# update task
+def update_task(request, id):
+    if request.method == 'POST':
+        try:
+            task = Task.objects.get(pk=id)
+            form = UpdateTaskForm(data=request.POST, instance=task)
+            form.save()
+            messages.success(request, 'Task has been updated successfully!')
+        except Exception as e:
+            messages.error(request, e)
+
+        return redirect('tasks:index')
+
+
+# retrieve all tasks
+def get_all_tasks(request):
+    context = {
+        'tasks': Task.objects.all(),
+        'create_task_form': CreateTaskForm(),
+        'update_task_form': UpdateTaskForm()
+    }
+    return render(request=request, template_name='tasks/index.html', context=context)
+
+
+# show single task
+def get_task(request, id):
+    task = Task.objects.filter(pk=id).first()
+
+    context = {
+        'task': task
+    }
+    return render(request=request, template_name='tasks/task-detail.html', context=context)
+
+
+# get task as json
+def get_task_json(request, id):
+    task = Task.objects.get(pk=id)
+    task_dict = {
+        'id': task.id,
+        'title': task.title,
+        'detail': task.detail,
+        'due_date': task.due_date
+    }
+
+    return JsonResponse(data=task_dict, safe=True)
